4fb34a2a995d3e69afd209b78b4898e349a855b3
Braifz <braian94lp@gmail.com>
2022-05-17 15:47:29 -0300

    change extension .js -> .jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import TodoApp from "./components/TodoApp";
+import Layout from "./components/Layout";
+
+function App() {
+  return (
+    <div>
+      <Layout>
+        <TodoApp />
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
