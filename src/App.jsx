82ed104fc5687789dff73d43ca5c1a9e68f76ad9
braifz <braian94lp@gmail.com>
2022-08-08 14:08:45 -0300

    add button for test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import TodoApp from "./components/TodoApp";
 import Layout from "./components/Layout";
+import { getTasks } from "./services/taskService";
 
 function App() {
   return (
     <div>
       <Layout>
         <TodoApp />
+        <button onClick={getTasks}> click</button>
       </Layout>
     </div>
   );
